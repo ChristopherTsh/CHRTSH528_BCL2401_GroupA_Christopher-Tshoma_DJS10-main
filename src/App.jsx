51eedb9a84765f9f8a5e1330c209b57f37071794
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-12 14:04:52 +0200

    "Added useState and useEffect hooks to fetch and display blog posts from API, with error handling"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 
 export default function App(){
 
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => setPosts(data))
+      .catch(error => setError(error.message));
+  }, []);
+
+  if (error) {
+    return {error}
+  }
   
   
   return (
+    <div>
+    <h1>blogPost</h1>
+    <ul>
+{posts.map(post => (
+<li key={post.id}>
+  <h2>{post.title}</h2>
+  <p>{post.body}</p>
+</li>
+))}
+</ul>
 
-  )
-
+  </div>
+);
 }
+
+ 
